ab38902fa3bddc2a60b61ef1e386c732d78ab4af
Ilya Elias Sidorov <ilya@shadowapi.com>
2025-07-16 17:23:48 +0300

    fix vite path alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig(async () => {
   const react = (await import('@vitejs/plugin-react')).default
+  const tsconfigPaths = (await import('vite-tsconfig-paths')).default
   return {
-    plugins: [react()],
+    plugins: [react(), tsconfigPaths()],
     server: {
       open: false,
     },
